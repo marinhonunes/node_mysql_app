4a3fb50ea95c2d6d44de9200665883fd189c978b
Mario Nunes <marionunes_azevedo@hotmail.com>
2024-12-02 23:37:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 3000;
+
+const connection = mysql.createConnection({
+    host: '137.131.181.161', 
+    user: 'usuario_node', 
+    password: 'senha123',
+    database: 'atividadefinal',
+    port: 3306
+});
+
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados: ' + err.stack);
+        return;
+    }
+    console.log('Conectado ao banco de dados como id ' + connection.threadId);
+});
+
+app.get('/musicas', (req, res) => {
+    connection.query('SELECT * FROM musica', (err, results) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
